6511b739bac84135b8a8c796f0c178402cfdd106
DuDo TM <78534390+Aleksio-DuDo@users.noreply.github.com>
2021-08-30 00:20:35 +0200

    Update v0.1

---
+++ Random Name Extension/nick gen.js
@@ -0,0 +1,8 @@
+document.getElementById('lol').onclick = name;
+    function name() { 
+
+    fetch("https://apis.kahoot.it/namerator")
+    .then(a=>a.json())
+    .then(b=>document.getElementById('nic').innerHTML="Your nick is: " + b.name)
+    }
+
